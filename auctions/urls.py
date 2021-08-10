85d7c030024e010e30b0ec4b781cbe09d243c753
AtharvaCM <necrons999@gmail.com>
2021-08-10 10:48:11 +0530

    active listing django

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("<int:listing_id>", views.display_listing, name="display_listing"),
     path("display_category", views.display_category, name="display_category"),
 
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -5,19 +5,36 @@ from .models import Listing
 
 
 def index(request):
-    listings = Listing.objects.filter(is_closed=False)
+    # listings = Listing.objects.filter(is_closed=False)
     template_name = 'auctions/index.html'
     context = {
-        "listings": listings,
+        # "listings": listings,
     }
     return render(request, template_name, context)
 
 
 def display_category(request):
-    category = request.POST["category"]
-    listings = Listing.objects.filter(category=category)
-    listings = set(listings)
+    category = request.POST.get('category', False)
+    if category == False:
+        listings = Listing.objects.filter(is_closed=False)
+    else:
+        listings = Listing.objects.filter(category=category)
+        listings = set(listings)
     context = {
         "listings": listings,
     }
-    return render(request, "auctions/index.html", context)
+    return render(request, "auctions/listing_category.html", context)
+
+
+def display_listing(request, listing_id):
+    listing = Listing.objects.get(pk=listing_id)
+    # is_listing_in_watchlist = request.user in listing.watchlist.all()
+    # comments = listing.comments.all()
+    is_owner = request.user.username == listing.owner.usernames
+    context = {
+        "listing": listing,
+        # "is_listing_in_watchlist": is_listing_in_watchlist,
+        "is_owner": is_owner,
+        # "comments": comments
+    }
+    return render(request, "auctions/listing_page.html", context)
