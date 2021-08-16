b8ddaf27e4a5632243936ec4321dd8c47ca9669a
AtharvaCM <necrons999@gmail.com>
2021-08-16 20:18:24 +0530

    ppt and slight changes

--- auctions/admin.py
+++ auctions/admin.py
@@ -3,6 +3,16 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, Listing, Bid
 
 # Register your models here.
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_closed', 'category', 'bid')
+
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('bid', 'user')
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Listing)
-admin.site.register(Bid)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
