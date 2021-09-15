867193e803c8dc6e74343bb0e9af22d75d26509a
AtharvaCM <necrons999@gmail.com>
2021-09-15 17:10:57 +0530

    profile changes

--- auctions/static/auctions/js/main.js
+++ auctions/static/auctions/js/main.js
@@ -6,9 +6,4 @@ if (document.readyState == "loading") {
 
 function ready() {
   console.log("doc ready");
-
-  var loadFile = function (event) {
-    var image = document.getElementById('output');
-    image.src = URL.createObjectURL(event.target.files[0]);
-  };
 }
