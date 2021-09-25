2933c634145654568f6d3f3cd50b4d3298d8a71f
AtharvaCM <necrons999@gmail.com>
2021-09-25 14:49:55 +0530

    reset template changes

--- auctions/urls.py
+++ auctions/urls.py
@@ -13,12 +13,12 @@ urlpatterns = [
 
     path('reset_password', auth_views.PasswordResetView.as_view(template_name='auctions/password_reset.html'),
          name='reset_password'),
-    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name='auctions/password_reset_sent.html'),
          name='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='auctions/password_reset_form.html'),
          name='password_reset_confirm'),
     path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(
-    ), name='password_reset_complete'),
+        template_name='auctions/password_reset_done.html'), name='password_reset_complete'),
 
     path('search/', views.SearchResultsView.as_view(), name='search_results'),
     path('about', views.about, name='about'),
